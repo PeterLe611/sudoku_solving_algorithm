8bdc27d79bb006ee1e74074fd3b457dc15a9a15a
DinhDinh-DumDum <khoi120520051@gmail.com>
2025-04-19 10:06:37 +0700

    Update DancingLinks.java

--- src/com/rmit/sudokusolver/algorithms/dancingLInks/DancingLinks.java
+++ src/com/rmit/sudokusolver/algorithms/dancingLInks/DancingLinks.java
@@ -10,8 +10,8 @@ import java.util.List;
 
         // DLX Node structure
         private static class Node {
-            Node left, right, up, down;
-            ColumnHeader header;
+            Node left, right, up, down;// 4 directional node
+            ColumnHeader header; // "root" of the linked list
 
             Node() {
                 this.left = this.right = this.up = this.down = this;
@@ -29,11 +29,11 @@ import java.util.List;
         }
 
         private ColumnHeader header;
-        private List<Node> solution;
-        private int[][] solutionBoard;
+        private List<Node> solution; // possible solution
+        private int[][] solutionBoard; // the board that have added all the solution node
 
         public DancingLinks() {
-            header = createDLXMatrix();
+            header = createDLXMatrix(); // create the Exact Cover Problem
             solution = new ArrayList<>();
         }
 
@@ -43,10 +43,10 @@ import java.util.List;
             ColumnHeader prev = root;
 
             // 4 types of constraints: cell, row, column, box
-            for (int i = 0; i < SIZE * SIZE * 4; i++) {
-                ColumnHeader col = new ColumnHeader(i);
-                col.left = prev;
-                prev.right = col;
+            for (int i = 0; i < SIZE * SIZE * 4; i++) { // first 81 columns: cell constrain
+                ColumnHeader col = new ColumnHeader(i); // first 81 columns: column constrain
+                col.left = prev;                        // first 81 columns: row constrain
+                prev.right = col;                       // first 81 columns: box constrain
                 prev = col;
             }
             prev.right = root;
