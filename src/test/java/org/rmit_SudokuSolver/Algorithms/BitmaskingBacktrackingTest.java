3fbd846314f08be606d155cc888ea1f52585d4e0
PeterLe611 <142322089+PeterLe611@users.noreply.github.com>
2025-05-04 11:30:12 +0700

    Update BitmaskingBacktrackingTest.java

--- src/test/java/org/rmit_SudokuSolver/Algorithms/BitmaskingBacktrackingTest.java
+++ src/test/java/org/rmit_SudokuSolver/Algorithms/BitmaskingBacktrackingTest.java
@@ -1,4 +1,42 @@
 package org.rmit_SudokuSolver.Algorithms;
 
+import org.junit.Test;
+import static org.junit.Assert.*;
+import org.rmit_SudokuSolver.Algorithms.Backtracking.BacktrackingSolver;
+import org.rmit_SudokuSolver.Utils.PuzzleLoader;
+import java.io.IOException;
+
 public class BitmaskingBacktrackingTest {
+
+    BacktrackingSolver solver = new BacktrackingSolver();
+
+    @Test
+    public void testEasyPuzzle() throws IOException {
+        int[][] board = PuzzleLoader.loadFromFile("src/main/resources/easy.txt");
+        long start = System.nanoTime();
+        boolean solved = solver.solve(board);
+        long end = System.nanoTime();
+        assertTrue(solved);
+        System.out.println("Backtracking (Easy): " + (end - start) / 1_000_000 + " ms");
+    }
+
+    @Test
+    public void testMediumPuzzle() throws IOException {
+        int[][] board = PuzzleLoader.loadFromFile("src/main/resources/medium.txt");
+        long start = System.nanoTime();
+        boolean solved = solver.solve(board);
+        long end = System.nanoTime();
+        assertTrue(solved);
+        System.out.println("Backtracking (Medium): " + (end - start) / 1_000_000 + " ms");
+    }
+
+    @Test
+    public void testHardPuzzle() throws IOException {
+        int[][] board = PuzzleLoader.loadFromFile("src/main/resources/hard.txt");
+        long start = System.nanoTime();
+        boolean solved = solver.solve(board);
+        long end = System.nanoTime();
+        assertTrue(solved);
+        System.out.println("Backtracking (Hard): " + (end - start) / 1_000_000 + " ms");
+    }
 }
